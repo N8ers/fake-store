832db2b525b6092e4e0ae74d1c8e35fd97060679
sheryak_n <nathanraysheryak@gmail.com>
2022-11-06 15:28:41 -0500

    wireup and query the db w/ thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,34 @@
 import { createStore, applyMiddleware, compose } from "redux"
 import thunk from "redux-thunk"
+import { getFirestore, collection, query, getDocs } from "firebase/firestore"
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGEING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+}
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig)
+export const db = getFirestore()
 
 /**
  * Reducer - a function tha ttakes a current state value and action object describing "what happened"
  * returns a new state value
  * (state, action) => newState
  */
-function searchReducer(state = { searchTerm: "", searchResults: [] }, action) {
+function searchReducer(state = { searchTerm: "", searchResults: {} }, action) {
   switch (action.type) {
     case "search/SEARCH_TERM":
       return { searchTerm: action.payload }
@@ -21,8 +43,17 @@ function searchReducer(state = { searchTerm: "", searchResults: [] }, action) {
 
 // Thunk Action Creator = create a function that gives access to state and allow you dispatch new actions
 export const fetchTheData = () => async (dispatch, getState) => {
-  const gotData = [{ name: "butterscotch" }, { name: "peppermint" }]
-  dispatch({ type: "search/SET_RESULTS", payload: gotData })
+  const collectionRef = collection(db, "categories")
+  const q = query(collectionRef)
+  const querySnapshot = await getDocs(q)
+
+  const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+    const { title, items } = docSnapshot.data()
+    acc[title.toLowerCase()] = items
+    return acc
+  }, {})
+
+  dispatch({ type: "search/SET_RESULTS", payload: categoryMap })
 }
 
 const store = createStore(
