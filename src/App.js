a7aecc014f5a7ca0b86e7b41ad0bd403f7984cee
sheryak_n <nathanraysheryak@gmail.com>
2022-11-17 08:30:31 -0500

    check auth state on page load - one approach

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
+import { useEffect } from "react"
 import { Route, Routes } from "react-router-dom"
+import { onAuthStateChanged } from "firebase/auth"
+
+import { auth } from "./firebase/firebaseHelpers"
 
 import NavBar from "./components/UI/NavBar/NavBar"
 
@@ -6,6 +10,12 @@ import Home from "./pages/Home/Home"
 import UserSettings from "./pages/UserSettings/UserSettings"
 
 function App() {
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      console.log(user)
+    })
+  }, [])
+
   return (
     <div>
       <NavBar />
