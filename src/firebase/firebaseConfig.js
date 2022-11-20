4a4ece683562ec6fdce728c6932387d54dd07817
sheryak_n <nathanraysheryak@gmail.com>
2022-11-20 10:20:28 -0500

    just checkin the keys

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,9 +1,7 @@
 export const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGEING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAd_oAZFCUzWC7TN2RDoMN83t5YeuSQF_g",
+  authDomain: "fake-store-c67ba.firebaseapp.com",
+  projectId: "fake-store-c67ba",
+  appId: "1:947364040375:web:72b26f15aa8babce064732",
+  measurementId: "G-4K6TM9XVPV",
 }
