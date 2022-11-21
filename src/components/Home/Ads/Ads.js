7fd1474214e063ab59a2e4ee07ce711fd7b07163
sheryak_n <nathanraysheryak@gmail.com>
2022-11-21 07:33:21 -0500

    add welcome/explanation banner

--- src/components/Home/Ads/Ads.js
+++ src/components/Home/Ads/Ads.js
@@ -7,6 +7,17 @@ function DefaultHome() {
     <Box sx={{ flexGrow: 1 }} align="center">
       <Grid container spacing={2} className={styles.container}>
         <Grid item xs={12} align="center">
+          <Box className={styles.banner}>
+            <h2>Welcome to a real Fake Store</h2>
+            <p>
+              Have you ever wanted the electrifying buzz of ordering things
+              online, but don't want to actually pay? Then come visit the Real
+              Fake Store, it's really fake!!
+            </p>
+            {/* <p>Learn more about the Real Fake store on our About page.</p> */}
+          </Box>
+        </Grid>
+        <Grid item xs={12} align="center">
           <Box className={styles.box}>
             <h2>Purple Thursday</h2>
             <p>
