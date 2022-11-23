4bde71f8e874efb6478075c4083eef0fd157e70d
sheryak_n <nathanraysheryak@gmail.com>
2022-11-23 18:34:32 -0500

    start componitizing skeletons and cards

--- src/components/Home/Ads/Ads.js
+++ src/components/Home/Ads/Ads.js
@@ -2,40 +2,25 @@ import { useEffect, useState } from "react"
 import {
   Box,
   Grid,
+  Card,
+  CardMedia,
+  CardActionArea,
+  CardContent,
+  Typography,
   Skeleton,
+  ImageList,
   ImageListItem,
-  ImageListItemBar,
-  IconButton,
 } from "@mui/material"
 
 import { Info } from "@mui/icons-material"
 
 import styles from "./Ads.module.css"
 
-const LargeImg1 = (
-  <ImageListItem sx={{ width: 175 }}>
-    <img src="/cartoon-candy.png" alt="" />
-    <ImageListItemBar
-      title="hey"
-      subtitle="hi"
-      actionIcon={
-        <IconButton
-          sx={{ color: "rgba(255, 255, 255, 0.54)" }}
-          aria-label={`info about ___`}
-        >
-          <Info />
-        </IconButton>
-      }
-    />
-  </ImageListItem>
-)
-
 function DefaultHome() {
   const [isBox1Loading, setIsBox1Loading] = useState(true)
   const [isBox2Loading, setIsBox2Loading] = useState(true)
   const [isBox3Loading, setIsBox3Loading] = useState(true)
   const [isBox4Loading, setIsBox4Loading] = useState(true)
-  const [isBox5Loading, setIsBox5Loading] = useState(true)
 
   const randomSecond = () => {
     return Math.floor(Math.random() * (1000 - 150) + 150)
@@ -54,10 +39,36 @@ function DefaultHome() {
     setTimeout(() => {
       setIsBox4Loading(false)
     }, randomSecond())
-    setTimeout(() => {
-      setIsBox5Loading(false)
-    }, randomSecond())
   }, [])
+
+  const itemMedium = (
+    <Grid item sx={{ justifyContent: "center" }}>
+      <Card>
+        <CardActionArea>
+          <CardMedia component="img" height="80" image="/cartoon-candy.png" />
+          <CardContent>
+            <Typography>Butter Scotch</Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Grid>
+  )
+
+  const itemSmall = (
+    <Grid item sx={{ justifyContent: "center" }}>
+      <Card>
+        <CardActionArea>
+          <CardMedia component="img" height="50" image="/cartoon-candy.png" />
+        </CardActionArea>
+      </Card>
+    </Grid>
+  )
+
+  const skeletonSmall = (
+    <Grid item>
+      <Skeleton variant="rounded" width={45} height={45} animation="wave" />
+    </Grid>
+  )
 
   return (
     <Box sx={{ flexGrow: 1 }} align="center">
@@ -96,7 +107,7 @@ function DefaultHome() {
         <Grid item xs={6} align="center">
           <Box className={styles.box}>
             <p>Recommeneded for you</p>
-            <Box sx={{ width: 300 }}>
+            <Box>
               {isBox2Loading ? (
                 <>
                   <Skeleton />
@@ -104,7 +115,13 @@ function DefaultHome() {
                   <Skeleton animation={false} />
                 </>
               ) : (
-                "Loaded"
+                <Box>
+                  <Grid container spacing={2}>
+                    {itemMedium}
+                    {itemMedium}
+                    {itemMedium}
+                  </Grid>
+                </Box>
               )}
             </Box>
           </Box>
@@ -114,49 +131,40 @@ function DefaultHome() {
             <p>keep shopping for</p>
             <Box sx={{ width: 150 }}>
               {isBox3Loading ? (
-                <>
-                  <Skeleton />
-                  <Skeleton animation="wave" />
-                  <Skeleton animation={false} />
-                </>
+                <Grid container spacing={2}>
+                  {skeletonSmall}
+                  {skeletonSmall}
+                  {skeletonSmall}
+                  {skeletonSmall}
+                </Grid>
               ) : (
-                "Loaded"
+                <Grid container spacing={2}>
+                  {itemSmall}
+                  {itemSmall}
+                  {itemSmall}
+                  {itemSmall}
+                </Grid>
               )}
             </Box>
           </Box>
         </Grid>
         <Grid item xs={3} align="center">
           <Box className={styles.box}>
-            <p>Early Purple Thursday deals</p>
+            <p>Early Purple Thursday Deals</p>
             <Box sx={{ width: 150 }}>
               {isBox4Loading ? (
-                <>
-                  <Skeleton />
-                  <Skeleton animation="wave" />
-                  <Skeleton animation={false} />
-                </>
-              ) : (
-                "Loaded"
-              )}
-            </Box>
-          </Box>
-        </Grid>
-        <Grid item xs={12} align="center">
-          <Box className={styles.box}>
-            <p>More items to explore</p>
-            <Box sx={{ width: 750 }}>
-              {isBox5Loading ? (
-                <>
-                  <Skeleton />
-                  <Skeleton animation="wave" />
-                  <Skeleton animation={false} />
-                </>
+                <Grid container spacing={2}>
+                  {skeletonSmall}
+                  {skeletonSmall}
+                  {skeletonSmall}
+                  {skeletonSmall}
+                </Grid>
               ) : (
                 <Grid container spacing={2}>
-                  <Grid item>{LargeImg1}</Grid>
-                  <Grid item>{LargeImg1}</Grid>
-                  <Grid item>{LargeImg1}</Grid>
-                  <Grid item>{LargeImg1}</Grid>
+                  {itemSmall}
+                  {itemSmall}
+                  {itemSmall}
+                  {itemSmall}
                 </Grid>
               )}
             </Box>
