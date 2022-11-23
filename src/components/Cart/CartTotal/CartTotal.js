3ea5c6c250c00baa2b88deb0c9f0125b077cb58c
sheryak_n <nathanraysheryak@gmail.com>
2022-11-23 07:09:00 -0500

    remove the card action area from cart

--- src/components/Cart/CartTotal/CartTotal.js
+++ src/components/Cart/CartTotal/CartTotal.js
@@ -1,20 +1,18 @@
-import { Card, CardActionArea, Box, Grid } from "@mui/material"
+import { Card, Box, Grid } from "@mui/material"
 
 function CartItem({ total }) {
   return (
     <Box sx={{ margin: "20px" }}>
       <Card sx={{ padding: "20px" }}>
-        <CardActionArea>
-          <Grid container spacing={2}>
-            <Grid item>
-              Cart Total:
-              {total.toLocaleString("en-US", {
-                style: "currency",
-                currency: "USD",
-              })}
-            </Grid>
+        <Grid container spacing={2}>
+          <Grid item>
+            Cart Total:
+            {total.toLocaleString("en-US", {
+              style: "currency",
+              currency: "USD",
+            })}
           </Grid>
-        </CardActionArea>
+        </Grid>
       </Card>
     </Box>
   )
